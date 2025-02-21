869898b417d4efcecec8b3616d73beefba8cc966
cmsaleel <116287263+cmsaleel@users.noreply.github.com>
2025-02-21 14:04:01 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,8 @@ st.write(
     " AI Chatboat for your assisstance"
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+openai.api_key = 'your-api-key'  # Replace with your OpenAI API key or use an environment variable
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
